a444ac239dac3c2cd3fd8e9b1445bae381b23bc0
carlosdiazz <c.diazadriann@gmail.com>
2022-09-12 18:57:48 -0400

    OKKKKKL

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,9 +2,9 @@ const {Sequelize} = require('sequelize');
 const {config} = require('../config/config');
 const setupModels = require('../db/models');
 
-//const USER_PRO = encodeURIComponent(config.dbPort_PRO);
+//const USER_PRO = encodeURIComponent(config.dbUser_PRO);
 const USER_DEV = encodeURIComponent(config.dbUser_DEV);
-const PASSWORD = encodeURIComponent(config.dbUser_PRO);
+const PASSWORD = encodeURIComponent(config.dbPass);
 //const URI_MYSQL = `mysql://${USER_PRO}:${PASSWORD}@${config.dbHost}:${config.dbPort_PRO}/${config.dbName}`;
 const URI_POSTGRES = `postgres://${USER_DEV}:${PASSWORD}@${config.dbHost}:${config.dbPort_DEV}/${config.dbName}`;
 
